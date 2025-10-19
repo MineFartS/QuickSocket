424c67140e7b9370d8a0c0c10207d848fedd3ca5
Phil Hunt <philiphuntiv2@gmail.com>
2025-10-19 17:32:21 -0400

    slight changes

--- quicksocketpy/__init__.py
+++ quicksocketpy/__init__.py
@@ -1,46 +1,60 @@
-import socket as _socket, struct, dill
-from typing import Generator
+from typing import Generator, TYPE_CHECKING
 
-def __socket(timeout:int=10):
-    s = _socket.socket(_socket.AF_INET, _socket.SOCK_STREAM)
+if TYPE_CHECKING:
+    from socket import socket as __socket
+
+def socket(timeout:int=10):
+    from socket import socket, AF_INET, SOCK_STREAM
+
+    s = socket(AF_INET, SOCK_STREAM)
+    
     s.settimeout(timeout)
+    
     return s
 
-class __conn:
+class conn:
 
-    def __init__(self, conn:_socket.socket):
-        self.__conn = conn
+    def __init__(self, conn:__socket):
+        
+        self.conn = conn
 
     def send(self, data):
+        from dill import dumps
+        from struct import pack
 
-        data = dill.loads(data)
+        data = dumps(data)
 
         # Pack the length into a 4-byte header (e.g., using '!' for network byte order, 'I' for unsigned int)
-        header = struct.pack('!I', len(data))
+        header = pack('!I', len(data))
 
         # Send the header
-        self.__conn.sendall(header)
+        self.conn.sendall(header)
 
         # Send the actual data
-        self.__conn.sendall(data)
+        self.conn.sendall(data)
 
     def recv(self):
+        from dill import loads
+        from struct import unpack
 
         # Unpack the length from the header
-        length = struct.unpack('!I', 
-            self.__conn.recv(4)
+        length = unpack('!I', 
+            self.conn.recv(4)
         )[0]
 
         # Receive the actual data based on the unpacked length
-        data = self.__conn.recv(length).decode('utf-8')
+        data = self.conn.recv(length).decode('utf-8')
 
-        return dill.loads(data)
+        return loads(data)
 
 class host:
 
-    def __init__(self, port:int):
-        self.bindings = ('127.0.0.1', port)
-        self.s = _socket.socket(_socket.AF_INET, _socket.SOCK_STREAM)
+    def __init__(self,
+        ip: str = '127.0.0.1',
+        port: int = 80
+    ):
+        self.bindings = (ip, port)
+        self.s = socket()
         self.start()
     
     def close(self):
@@ -57,18 +71,17 @@ class host:
             self.started = False
             return
 
-    def listen(self):
+    def listen(self) -> Generator[conn]:
         while True:
-            try:
-                yield __conn(self.s.accept()[0])
-            except OSError:
-                pass
+            yield conn(self.s.accept()[0])
 
-def client(ip:str, port:int):
+def client(
+    ip: str = '127.0.0.1',
+    port: int = 80
+):
     try:
-        conn_ = __socket()
+        conn_ = socket()
         conn_.connect((ip, port))
-        return __conn(conn_)
+        return conn(conn_)
     except:
         return None
-
